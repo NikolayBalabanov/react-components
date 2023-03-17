f60076f9fd51d5d31afe7a6f8f69f896581ef912
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-03-17 16:47:21 +0300

    feat: add cards with its styles

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <h2 className="font-bold">About page</h2>
+      <div className="container mx-auto max-w-2xl pt-5 flex flex-col items-center">
+        <h2 className="font-bold text-3xl mb-3">About page</h2>
         <img
+          className="rounded"
           src="https://media.tenor.com/-cSWvXO8UUAAAAAC/leonardo-dicaprio-leo.gif"
           alt="leo gif"
         />
