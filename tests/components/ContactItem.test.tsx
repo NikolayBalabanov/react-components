7c40fb69f8b988f80b50a326db9c16a5d1e40284
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-03-24 17:10:42 +0300

    refactor: remove unuseful imports

--- tests/components/ContactItem.test.tsx
+++ tests/components/ContactItem.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import ContactItem from '../../src/components/ContactItem';
 
 const mockData = {
