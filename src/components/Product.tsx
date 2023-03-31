f97f326c82952e1bbfa39623caa02e6d164b450c
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-03-31 16:11:28 +0300

    feat: rewrite Product.tsx to functional component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 interface IProduct {
   id: number;
@@ -18,45 +18,34 @@ interface IProductProps {
   product: IProduct;
 }
 
-interface IProductState {
-  isOpen: boolean;
+function Product({ product }: IProductProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const btnBgClassName = isOpen ? 'bg-yellow-400' : 'bg-blue-400';
+  const btnClasses = ['py-2 px-4 boder rounded', btnBgClassName];
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img className="w-3/6" src={product.thumbnail} alt={product.title} />
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price}</p>
+      <button
+        className={btnClasses.join(' ')}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        type="button"
+      >
+        {isOpen ? 'Hide Details' : 'Show Details'}
+      </button>
+      {isOpen && (
+        <div>
+          <p>{product.description}</p>
+          <p>
+            Rate:<span style={{ fontWeight: 'bold' }}>{product.rating}</span>
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default class Product extends Component<IProductProps, IProductState> {
-  constructor(props: IProductProps) {
-    super(props);
-    this.state = { isOpen: false };
-  }
-  render() {
-    const btnBgClassName = this.state.isOpen ? 'bg-yellow-400' : 'bg-blue-400';
-    const btnClasses = ['py-2 px-4 boder rounded', btnBgClassName];
-    return (
-      <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-        <img className="w-3/6" src={this.props.product.thumbnail} alt={this.props.product.title} />
-        <p>{this.props.product.title}</p>
-        <p className="font-bold">{this.props.product.price}</p>
-        <button
-          className={btnClasses.join(' ')}
-          onClick={() => {
-            this.setOpen();
-          }}
-          type="button"
-        >
-          {this.state.isOpen ? 'Hide Details' : 'Show Details'}
-        </button>
-        {this.state.isOpen && (
-          <div>
-            <p>{this.props.product.description}</p>
-            <p>
-              Rate:<span style={{ fontWeight: 'bold' }}>{this.props.product.rating}</span>
-            </p>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  private setOpen = () => {
-    this.setState((prev) => ({ isOpen: !prev.isOpen }));
-  };
-}
+export default Product;
