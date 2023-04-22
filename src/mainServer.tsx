95e532b953dd0ec2ae4fdfa165faa602bcc66973
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-04-22 16:21:22 +0300

    feat: add prod and dev mode for ssr

--- src/mainServer.tsx
+++ src/mainServer.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-export function render(url: string, opts: object) {
+export default function render(url: string, opts: object) {
   const stream = renderToPipeableStream(
     <Provider store={store}>
       <StaticRouter location={url}>
